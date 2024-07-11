f9cb1f0c9eefb0a64550b391bac3888066f518dc
Umar Iqbal <umar.iqbal@staples.com>
2024-07-11 21:16:54 +0500

    Handle exceptions.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { BsFillExclamationCircleFill } from "react-icons/bs";
+import { isEmpty } from 'lodash';
 
 import EmailStatsDoughnutChart from './components/graphs/CircularAnalyticsGraph';
 import { transformResponseObject } from './utils/client-utils';
 import Loader from './components/common/Loader';
+
 
 function App() {
   const [analyticsData, setAnalyticsData] = useState([]);
@@ -29,13 +32,31 @@ function App() {
   return (
     <div className="card">
       {loading ? <Loader /> :
-        <EmailStatsDoughnutChart
-          total={total}
-          safetosend={safetosend}
-          valid={valid}
-          invalid={invalid}
-          trap={trap}
-        />}
+        <>
+          {!isEmpty(analyticsData) ? <EmailStatsDoughnutChart
+            total={total}
+            safetosend={safetosend}
+            valid={valid}
+            invalid={invalid}
+            trap={trap}
+          /> : <div
+            style={{
+              backgroundColor: "#FF8D8D",
+              color: "black",
+              fontWeight: "600",
+              padding: "10px 20px",
+              margin: "10px",
+              borderRadius: "20px",
+              display: "inline-block",
+              boxShadow: "7px 5px"
+            }}>
+            <div style={{ display: "flex" }}>
+              <span style={{ display: "inline-block", marginRight: "5px", paddingTop: "4px" }}><BsFillExclamationCircleFill size={20} /></span>
+              <span style={{ padding: "4px 0" }}>No Safe2Send analytics found!</span>
+            </div>
+          </div>}
+        </>
+      }
     </div>
   )
 }
