fca6e9a5e2aae2f762db3e0321c76b60d7160cfd
Umar Iqbal <umar.iqbal@staples.com>
2024-07-11 21:02:53 +0500

    Add loader component.

---
+++ src/components/common/Loader.jsx
@@ -0,0 +1,17 @@
+import { Puff } from 'react-loader-spinner'
+
+function Loader() {
+    return (
+        <Puff
+            visible={true}
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="puff-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+        />
+    )
+}
+
+export default Loader
