50cbbf15d6b558c4f67f7a4ac3e943ca49cb11a0
Umar Iqbal <umar.iqbal@staples.com>
2024-07-11 21:03:54 +0500

    Add method to transform reponse obj.

--- src/utils/client-utils.js
+++ src/utils/client-utils.js
@@ -7,3 +7,15 @@ export function getAllQueryParams() {
     return queryParams;
 }
 
+export const transformResponseObject = (data) => {
+    if(typeof data !== "undefined") {
+        const obj = {};
+        let total = 0;
+        data?.forEach(item => {
+            obj[item.code] = item.matches;
+            total = total + item.matches || 0;
+        });
+        return { ...obj, total };
+    }
+    return {};
+}
