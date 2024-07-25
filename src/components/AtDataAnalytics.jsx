d01e52260b5f0c301afb32ede1abb9c57447797e
Umar Iqbal <email.umariqbal@gmail.com>
2024-07-25 16:39:12 +0500

    Add AtDataAnalytics Graph component.

---
+++ src/components/AtDataAnalytics.jsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Typography } from '@mui/material';
+import { isEmpty } from 'lodash';
+
+import appConfig from '../utils/constants';
+import Loader from './common/Loader';
+import AtDataAnalyticsGraph from './graphs/AtDataAnalyticsGraph';
+import MessageBanner from './common/MessageBanner';
+
+function AtDataAnalytics() {
+    const [atDataAnalytics, setAtDataAnalytics] = useState();
+    const [loading, setLoading] = useState(false);
+    const baseURL = appConfig?.services?.baseURL;
+
+    useEffect(() => {
+        const fetchAtDataAnalytics = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(`${baseURL}/v2/dashboardApi/update-dashboard`);
+                const data = response?.data?.payload?.data;
+                if (!isEmpty(data)) {
+                    setAtDataAnalytics(data?.statusCounts);
+                }
+            } catch (err) {
+                console.log('📌 ~ Fetch AtData Analytics ~ err: ', err);
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchAtDataAnalytics();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <>
+            <div style={{ margin: '20px 0' }}>
+                <Typography variant='h3'>At Data Analytics</Typography>
+            </div>
+            <hr />
+            <div
+                style={{
+                    backgroundColor: '#ffffff',
+                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                    minWidth: '600px',
+                    padding: '30px',
+                    minHeight: 'fit-content',
+                    borderRadius: '20px',
+                    position: 'relative',
+                    marginTop: '20px'
+                }}>
+                {loading && (
+                    <div style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        zIndex: 1
+                    }}>
+                        <Loader />
+                    </div>
+                )}
+                {
+                    !isEmpty(atDataAnalytics) ?
+                        <>
+                            <AtDataAnalyticsGraph
+                                serverError={atDataAnalytics?.['500']}
+                                noResponse={atDataAnalytics?.['400']}
+                                valid={atDataAnalytics?.['200']}
+                            />
+                        </> :
+                        loading ?
+                            <div style={{ height: '400px' }} />
+                            : <MessageBanner title={'No At Data Analytics found!'} bg={'#FF6F6F'} txtColor={'#FFFFFF'} />
+                }
+            </div>
+        </>
+    )
+}
+
+export default AtDataAnalytics
---
+++ src/components/graphs/AtDataAnalyticsGraph.jsx
@@ -0,0 +1,83 @@
+import { Doughnut } from "react-chartjs-2";
+
+// eslint-disable-next-line react/prop-types
+function AtDataAnalyticsGraph({ valid = 0, serverError = 0, noResponse = 0 }) {
+    const total = valid + serverError + noResponse;
+    const data = {
+        labels: ['Valid', 'Server Error', 'No Response'],
+        datasets: [{
+            label: 'Email Stats',
+            data: [valid, serverError, noResponse],
+            backgroundColor: ['#4caf50', '#f44336', '#ff9800'],
+            hoverOffset: 4,
+        }],
+    };
+
+    const options = {
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function (tooltipItem) {
+                        const value = tooltipItem.raw;
+                        const percentage = ((value / total) * 100).toFixed(2);
+                        return `${value} (${percentage}%)`;
+                    },
+                },
+            },
+        },
+    };
+    return (
+        <div
+            style={{
+                backgroundColor: "#ffffff",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                display: "inline-block",
+                minWidth: "600px",
+                minHeight: "250px",
+                padding: "30px",
+                borderRadius: "20px",
+                color: "#333",
+            }}>
+            <h3
+                style={{
+                    marginTop: 0,
+                    marginBottom: "20px",
+                    fontWeight: "700",
+                    textAlign: "center",
+                    color: "#fff",
+                    backgroundColor: "#000",
+                    padding: "15px 0",
+                    borderRadius: "20px"
+                }
+                }>
+                At Data Analytics
+            </h3>
+            <div style={{ display: 'flex', alignItems: 'center', }}>
+                <div style={{
+                    width: 300,
+                    height: 300,
+                    marginRight: 40,
+                }}>
+                    <Doughnut data={data} options={options} />
+                </div>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: "10px" }}>
+                        <div style={{ width: 20, height: 20, backgroundColor: '#8bc34a', marginRight: 10, borderRadius: "3px" }}></div>
+                        <div><b>Valid:</b> {valid} ({((valid / total) * 100).toFixed(2)}%)</div>
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: "10px" }}>
+                        <div style={{ width: 20, height: 20, backgroundColor: '#f44336', marginRight: 10, borderRadius: "3px" }}></div>
+                        <div><b>Server Error:</b> {serverError} ({((serverError / total) * 100).toFixed(2)}%)</div>
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: "10px" }}>
+                        <div style={{ width: 20, height: 20, backgroundColor: '#ff9800', marginRight: 10, borderRadius: "3px" }}></div>
+                        <div><b>No Response:</b> {noResponse} ({((noResponse / total) * 100).toFixed(2)}%)</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AtDataAnalyticsGraph
