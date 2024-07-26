084bb06d530b82acc734f60b485e3801fcd1d1d7
Umar Iqbal <email.umariqbal@gmail.com>
2024-07-26 16:07:55 +0500

    Add container for checkout-form

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -1,5 +1,6 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import { Container } from "@mui/material";
 import CheckoutForm from "../components/form/StripForm";
 
 const stripePromise = loadStripe("pk_test_51NZrH0C0wgKISC6Qdz5K1tfa7ZzPdrOfIS2tiSlIIMF4gyvh53WtJGRc7BepwUxBaaq6RbzMuvQeRUEUfDBzC9IQ00ZpsChaX4");
@@ -12,9 +13,11 @@ function Checkout() {
     appearance: {},
   };
   return (
-    <Elements stripe={stripePromise} options={options}>
-     <CheckoutForm amount={50}/>
-    </Elements>
+    <Container>
+      <Elements stripe={stripePromise} options={options}>
+        <CheckoutForm amount={50} />
+      </Elements>
+    </Container>
   )
 }
 
