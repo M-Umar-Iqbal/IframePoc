ae85c8f8f52b0a75af774eced48ae6a7554a5b42
Umar Iqbal <email.umariqbal@gmail.com>
2024-07-26 14:53:07 +0500

    Add services validations page.

---
+++ src/pages/services-validations.jsx
@@ -0,0 +1,16 @@
+import Grid from '@mui/material/Grid';
+import AtDataServicesValidationsForm from "../components/form/AtDataServicesValidationsForm";
+
+function ServicesValidations() {
+    return (
+        <div style={{ padding: "30px" }}>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={4}>
+                    <AtDataServicesValidationsForm />
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+export default ServicesValidations;
