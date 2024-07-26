2a437991cab547b5d8555ddab9e92f1650439806
Umar Iqbal <email.umariqbal@gmail.com>
2024-07-26 12:45:51 +0500

    Add checkout page.

---
+++ src/pages/checkout.jsx
@@ -0,0 +1,21 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "../components/form/StripForm";
+
+const stripePromise = loadStripe("pk_test_51NZrH0C0wgKISC6Qdz5K1tfa7ZzPdrOfIS2tiSlIIMF4gyvh53WtJGRc7BepwUxBaaq6RbzMuvQeRUEUfDBzC9IQ00ZpsChaX4");
+function Checkout() {
+  const options = {
+    mode: 'payment',
+    payment_method_types: ["card"],
+    currency: 'usd',
+    amount: 100,
+    appearance: {},
+  };
+  return (
+    <Elements stripe={stripePromise} options={options}>
+     <CheckoutForm amount={50}/>
+    </Elements>
+  )
+}
+
+export default Checkout
